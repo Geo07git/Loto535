339328c8dd5559093da3934e2029b78d076612e2
Geo07git <110935974+Geo07git@users.noreply.github.com>
2025-03-11 15:13:19 +0200

    Update 535gre.py

--- 535gre.py
+++ 535gre.py
@@ -257,4 +257,6 @@ tz = pytz.timezone('Europe/Bucharest')
 now = datetime.now(tz).strftime("%d-%m-%Y") #%H:%M:%S %Z")
 #st.write(f"🕒 Actualizat pentru tragerea din {now} ora 15.00")
 
-st.write(f"🕒 Baza de date a fost actualizata pentru tragerea din {now} ora 15.00")
+#st.write(f"🕒 Baza de date a fost actualizata pentru tragerea din {now} ora 15.00") 
+
+st.writte(f"Seeviciul este în mentenanta") 
