4d9b9c0f226f0b31cd4fa1cbbfa7a78040a9cbfe
Geo07git <110935974+Geo07git@users.noreply.github.com>
2025-03-11 21:52:37 +0200

    Update 535gre.py

--- 535gre.py
+++ 535gre.py
@@ -259,4 +259,4 @@ now = datetime.now(tz).strftime("%d-%m-%Y") #%H:%M:%S %Z")
 
 #st.write(f"🕒 Baza de date a fost actualizata pentru tragerea din {now} ora 15.00") 
 
-st.write(f"## Seviciul este în mentenanta") 
+st.write(f"## Serviciul este în mentenanta") 
