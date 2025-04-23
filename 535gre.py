df24bb6429a7f02659bf510c32db9e4b5fefe48b
Geo07git <110935974+Geo07git@users.noreply.github.com>
2025-04-23 15:30:46 +0300

    Update 535gre.py

--- 535gre.py
+++ 535gre.py
@@ -79,7 +79,7 @@ st.subheader("Selectează și încarcă baza de date")
 # 🔹 Etichete personalizate pentru fișiere
 file_labels = {
     #"Italia - WinForLife": "itawin2y.csv",
-    "Grecie - Extra 5": "535.csv",
+    "Grecia - Extra 5": "535.csv",
     "Polonia - Kaskada": "kaskada.csv",
     "Romania 6/49": "loto649.csv"
 }
